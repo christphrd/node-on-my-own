012d636e1f8fd4b45498aeb6c3bf4e7bcbd37891
github username <diep.christopher@gmail.com>
2018-05-30 21:59:34 -0400

    made routing on nodejs

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-let http = require('http');
+//let http = require('http');
 
+//1. First try
 //create a server object
 /*http.createServer(function (request, response) {
   response.write('Hello World!');
@@ -12,11 +13,34 @@ let http = require('http');
 //see more built-in modules here: https://nodejs.org/dist/latest-v8.x/docs/api/
 
 
-//HTML Response instead of plain text. Only difference http header
-http.createServer(function (request, response) {
+//2. HTML Response instead of plain text. Only difference http header
+/*http.createServer(function (request, response) {
   response.writeHead(200, {'Content-Type': 'text/html'}); //http header
   response.write('<h1>Hello World! This is an HTML response this time.</h1>');
   response.end()
 }).listen(3000, function(){
   console.log("server start at port 3000 just because")
+})*/
+
+
+//3. Adding routing
+let http = require('http');
+
+//create a server object
+http.createServer(function(request, response) {
+  response.writeHead(200, {'Content-Type': 'text/html'});
+
+  let url = request.url;
+  if (url === '/about') {
+    response.write('<h4>about page stuff</h4>');
+    response.end();
+  } else if (url === '/contact') {
+    response.write('<h3>contact me through email only</h3>');
+    response.end();
+  } else {
+    response.write('<h1>Hello World</h1><p>this takes any route</p>');
+    response.end();
+  }
+}).listen(3000, function(){
+  console.log("server start at port 3000. conventional")
 })
