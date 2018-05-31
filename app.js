a929f4fb7f59380832864258501a5f899657da81
github username <diep.christopher@gmail.com>
2018-05-30 21:40:43 -0400

    HTML response instead of plain text. the difference is a header

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
 let http = require('http');
 
 //create a server object
-http.createServer(function (request, response) {
+/*http.createServer(function (request, response) {
   response.write('Hello World!');
   response.end();
 }).listen(3000, function(){
   console.log("server start at port 3000"); //the server object listens on port 3000
-})
+})*/
 
 //http is the built in module of Node JS through which data is transferred over HTTP protocols
 //see more built-in modules here: https://nodejs.org/dist/latest-v8.x/docs/api/
+
+
+//HTML Response instead of plain text. Only difference http header
+http.createServer(function (request, response) {
+  response.writeHead(200, {'Content-Type': 'text/html'}); //http header
+  response.write('<h1>Hello World! This is an HTML response this time.</h1>');
+  response.end()
+}).listen(3000, function(){
+  console.log("server start at port 3000 just because")
+})
